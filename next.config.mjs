9beb1aa6b01691c48428e5c2f29b54a4e4b0f242
Brian Lai <brianlai30@outlook.com>
2024-12-21 22:32:30 -0800

    MAJOR: Upgrade to NextJS 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
   basePath: process.env.NODE_ENV === 'development' ? undefined : '/next-master',
   images: {
     unoptimized: true,
