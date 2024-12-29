e36cddc1e27ef312c267831362c26c0554bde8ed
Brian Lai <brianlai30@outlook.com>
2024-12-29 15:56:38 -0800

    Inital bento grid for details page

--- app/monster/[name]/page.tsx
+++ app/monster/[name]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 import Image from 'next/image'
 import { pokeapi } from '@/lib/providers'
+import { StatsRadarChart } from '@/components/StatsRadarChart'
 
 export async function generateStaticParams() {
   const speciesList = await pokeapi.getPokemonSpeciesList({
@@ -72,80 +73,87 @@ export default async function Page({
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-4xl font-bold">
-        {
-          species.names.filter(
-            (nameResource) => nameResource.language.name === language
-          )[0].name
-        }
-      </h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-light">
+          {
+            species.names.filter(
+              (nameResource) => nameResource.language.name === language
+            )[0].name
+          }
+        </h1>
 
-      <Image
-        src={imageUrl}
-        alt={pokemon.name}
-        width={128}
-        height={128}
-        priority
-        className="border"
-      />
+        <div className="grid grid-cols-6 grid-rows-4 gap-4">
+          <section className="col-span-1 row-span-1 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
+            <Image
+              src={imageUrl}
+              alt={pokemon.name}
+              width={128}
+              height={128}
+              priority
+              className="border"
+            />
+          </section>
 
-      <section>
-        <h2 className="text-2xl font-semibold">Types</h2>
-        <ul className="list-disc pl-5">
-          {pokemon.types.map((type) => (
-            <li key={type.type.name}>{type.type.name}</li>
-          ))}
-        </ul>
-      </section>
+          <section className="col-span-2 row-span-1 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
+            <h2 className="text-2xl font-extralight">Types</h2>
+            <ul className="list-disc pl-5">
+              {pokemon.types.map((type) => (
+                <li key={type.type.name}>{type.type.name}</li>
+              ))}
+            </ul>
+          </section>
 
-      <section>
-        <h2 className="text-2xl font-semibold">Abilities</h2>
-        <ul className="list-disc pl-5">
-          {pokemon.abilities.map((ability) => (
-            <li key={ability.ability.name}>
-              {ability.ability.name + (ability.is_hidden ? ' (hidden)' : '')}
-            </li>
-          ))}
-        </ul>
-      </section>
+          <section className="col-span-2 row-span-1 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
+            <h2 className="text-2xl font-extralight">Gender ratio</h2>
+            <ul className="list-disc pl-5">
+              <li className="text-blue-800 dark:text-blue-200">87.5% male</li>
+              <li className="text-pink-800 dark:text-pink-200">12.5% female</li>
+            </ul>
+          </section>
 
-      <section>
-        <h2 className="text-2xl font-semibold">Stats</h2>
-        <ul className="list-disc pl-5">
-          {pokemon.stats.map((stat) => (
-            <li key={stat.stat.name}>
-              <p>
-                {stat.stat.name}: {stat.base_stat}
-              </p>
-              <meter value={stat.base_stat} min={0} max={255} />
-            </li>
-          ))}
-        </ul>
-      </section>
+          <section className="col-span-2 row-span-2 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
+            <h2 className="text-2xl font-extralight">Abilities</h2>
+            <ul className="list-disc pl-5">
+              {pokemon.abilities.map((ability) => (
+                <li key={ability.ability.name}>
+                  {ability.ability.name +
+                    (ability.is_hidden ? ' (hidden)' : '')}
+                </li>
+              ))}
+            </ul>
+          </section>
 
-      <section>
-        <h2 className="text-2xl font-semibold">Moves</h2>
-        <ul className="list-disc pl-5">
-          {pokemon.moves.map((move) => (
-            <li key={move.move.name}>
-              {move.move.name}
-              {/* <ul className="list-disc pl-5">
-                {move.version_group_details.map((version) => (
-                  <li
-                    key={`${version.version_group.name}_${version.move_learn_method.name}_${version.level_learned_at ?? 0}`}
-                  >
-                    {version.version_group.name} (
-                    {version.move_learn_method.name})
-                    {version.move_learn_method.name === 'level-up' && (
-                      <p>Learned at level {version.level_learned_at}</p>
-                    )}
-                  </li>
-                ))}
-              </ul> */}
-            </li>
-          ))}
-        </ul>
-      </section>
+          <section className="col-span-2 row-span-2 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
+            <h2 className="text-2xl font-extralight">Stats</h2>
+            <ul className="list-disc pl-5">
+              {pokemon.stats.map((stat) => (
+                <li key={stat.stat.name}>
+                  <p>
+                    {stat.stat.name}: {stat.base_stat}
+                  </p>
+                  <meter value={stat.base_stat} min={0} max={255} />
+                </li>
+              ))}
+            </ul>
+          </section>
+
+          <section className="col-span-2 row-span-2 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
+            <h2 className="text-2xl font-extralight">Stats</h2>
+            <StatsRadarChart />
+          </section>
+
+          {/* <section className="col-span-2 dark:bg-zinc-900 p-2 rounded-lg">
+            <h2 className="text-2xl font-extralight">Moves</h2>
+            <ul className="list-disc pl-5">
+              {pokemon.moves.map((move) => (
+                <li key={move.move.name}>
+                  {move.move.name}
+                </li>
+              ))}
+            </ul>
+          </section> */}
+        </div>
+      </div>
     </div>
   )
 }
