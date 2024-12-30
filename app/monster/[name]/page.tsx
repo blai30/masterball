9ace44b81af1106192a34e282abceb23f919198d
Brian Lai <brianlai30@outlook.com>
2024-12-29 22:33:55 -0800

    Update details page with bento template

--- app/monster/[name]/page.tsx
+++ app/monster/[name]/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next'
 import Image from 'next/image'
 import { pokeapi } from '@/lib/providers'
-import { StatsRadarChart } from '@/components/StatsRadarChart'
 
 export async function generateStaticParams() {
   const speciesList = await pokeapi.getPokemonSpeciesList({
@@ -72,86 +71,95 @@ export default async function Page({
   const imageUrl = `https://resource.pokemon-home.com/battledata/img/pokei128/icon${imageId}_f00_s0.png`
 
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-4xl font-light">
-          {
-            species.names.filter(
-              (nameResource) => nameResource.language.name === language
-            )[0].name
-          }
-        </h1>
-
-        <div className="grid grid-cols-6 grid-rows-4 gap-4">
-          <section className="col-span-1 row-span-1 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
-            <Image
-              src={imageUrl}
-              alt={pokemon.name}
-              width={128}
-              height={128}
-              priority
-              className="border"
-            />
-          </section>
-
-          <section className="col-span-2 row-span-1 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
-            <h2 className="text-2xl font-extralight">Types</h2>
-            <ul className="list-disc pl-5">
-              {pokemon.types.map((type) => (
-                <li key={type.type.name}>{type.type.name}</li>
-              ))}
-            </ul>
-          </section>
-
-          <section className="col-span-2 row-span-1 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
-            <h2 className="text-2xl font-extralight">Gender ratio</h2>
-            <ul className="list-disc pl-5">
-              <li className="text-blue-800 dark:text-blue-200">87.5% male</li>
-              <li className="text-pink-800 dark:text-pink-200">12.5% female</li>
-            </ul>
-          </section>
-
-          <section className="col-span-2 row-span-2 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
-            <h2 className="text-2xl font-extralight">Abilities</h2>
-            <ul className="list-disc pl-5">
-              {pokemon.abilities.map((ability) => (
-                <li key={ability.ability.name}>
-                  {ability.ability.name +
-                    (ability.is_hidden ? ' (hidden)' : '')}
-                </li>
-              ))}
-            </ul>
-          </section>
-
-          <section className="col-span-2 row-span-2 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
-            <h2 className="text-2xl font-extralight">Stats</h2>
-            <ul className="list-disc pl-5">
-              {pokemon.stats.map((stat) => (
-                <li key={stat.stat.name}>
-                  <p>
-                    {stat.stat.name}: {stat.base_stat}
-                  </p>
-                  <meter value={stat.base_stat} min={0} max={255} />
-                </li>
-              ))}
-            </ul>
-          </section>
-
-          <section className="col-span-2 row-span-2 rounded-lg p-2 bg-zinc-100 dark:bg-zinc-900">
-            <h2 className="text-2xl font-extralight">Stats</h2>
-            <StatsRadarChart />
-          </section>
-
-          {/* <section className="col-span-2 dark:bg-zinc-900 p-2 rounded-lg">
-            <h2 className="text-2xl font-extralight">Moves</h2>
-            <ul className="list-disc pl-5">
-              {pokemon.moves.map((move) => (
-                <li key={move.move.name}>
-                  {move.move.name}
-                </li>
-              ))}
-            </ul>
-          </section> */}
+    <div className="container mx-auto flex flex-col gap-8">
+      <div className="flex flex-row items-end justify-between">
+        <div className="flex flex-col gap-4 items-start">
+          <h2 className="text-base/7 font-semibold text-indigo-400">
+            {species.id}
+          </h2>
+          <h1 className="text-4xl font-semibold tracking-tight dark:text-white sm:text-5xl">
+            {
+              species.names.filter(
+                (nameResource) => nameResource.language.name === language
+              )[0].name
+            }
+          </h1>
+        </div>
+        <Image
+          src={imageUrl}
+          alt={species.name}
+          width={128}
+          height={128}
+          priority
+          className="h-full object-scale-down"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-6 lg:grid-rows-2">
+        <div className="flex lg:col-span-4">
+          <div className="overflow-hidden rounded-lg bg-zinc-900 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]">
+            <div className="p-10 gap-2 flex flex-col">
+              <h3 className="text-sm/4 font-semibold text-gray-400">
+                Releases
+              </h3>
+              <p className="text-lg font-medium tracking-tight text-white">
+                Push to deploy
+              </p>
+              <p className="text-sm/6 text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                gravida justo et nulla efficitur, maximus egestas sem
+                pellentesque.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex lg:col-span-2">
+          <div className="overflow-hidden rounded-lg bg-zinc-900 lg:rounded-tr-[2rem]">
+            <div className="p-10 gap-2 flex flex-col">
+              <h3 className="text-sm/4 font-semibold text-gray-400">
+                Integrations
+              </h3>
+              <p className="text-lg font-medium tracking-tight text-white">
+                Connect your favorite tools
+              </p>
+              <p className="text-sm/6 text-gray-400">
+                Curabitur auctor, ex quis auctor venenatis, eros arcu rhoncus
+                massa.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex lg:col-span-2">
+          <div className="overflow-hidden rounded-lg bg-zinc-900 lg:rounded-bl-[2rem]">
+            <div className="p-10 gap-2 flex flex-col">
+              <h3 className="text-sm/4 font-semibold text-gray-400">
+                Security
+              </h3>
+              <p className="text-lg font-medium tracking-tight text-white">
+                Advanced access control
+              </p>
+              <p className="text-sm/6 text-gray-400">
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex lg:col-span-4">
+          <div className="overflow-hidden rounded-lg bg-zinc-900 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]">
+            <div className="p-10 gap-2 flex flex-col">
+              <h3 className="text-sm/4 font-semibold text-gray-400">
+                Performance
+              </h3>
+              <p className="text-lg font-medium tracking-tight text-white">
+                Lightning-fast builds
+              </p>
+              <p className="text-sm/6 text-gray-400">
+                Sed congue eros non finibus molestie. Vestibulum euismod augue
+                vel commodo vulputate. Maecenas at augue sed elit dictum
+                vulputate.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
