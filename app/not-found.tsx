b37d966969cfbde9730b1216598d0f8b657dc079
Brian Lai <brianlai30@outlook.com>
2024-12-31 14:05:02 -0800

    Update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,13 +2,13 @@
 
 export default function NotFound() {
   return (
-    <div className="container mx-auto flex flex-1 flex-col items-center justify-center">
-      <p className="p-4 text-center text-3xl font-medium text-pretty">
-        OOPSIE WOOPSIE!!
-      </p>
-      <p className="p-4 text-center text-2xl leading-12 font-light text-pretty">
-        Uwu We made a f*cky wucky!! A wittle f*cko boingo! The code moneys at
-        our headquarters are working VEWY HAWD to fix this!
+    <div className="container mx-auto flex flex-1 flex-col items-center justify-center gap-4">
+      <h1 className="p-8 text-center text-4xl font-bold text-pretty">
+        404 Page Not Found
+      </h1>
+      <p className="px-8 text-center text-xl leading-12 font-light text-pretty">
+        OOPSIE WOOPSIE!! Uwu We made a f*cky wucky!! A wittle f*cko boingo! The
+        code moneys at our headquarters are working VEWY HAWD to fix this!
       </p>
     </div>
   )
