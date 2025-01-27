d177ce3120db157ca5d98b604cb48c7156afe7d5
Brian Lai <brianlai30@outlook.com>
2025-01-26 19:58:35 -0800

    Revert to old monster card design

--- components/MonsterCard.tsx
+++ components/MonsterCard.tsx
@@ -46,40 +46,26 @@ export default async function MonsterCard({
   return (
     <Link
       href={`/monster/${species.name}`}
-      className="group flex aspect-square flex-col items-center justify-between overflow-visible"
+      className="group flex flex-col items-center justify-between overflow-visible"
     >
-      <div className="relative flex size-full flex-col items-center justify-center rounded-md">
-        <Image
-          src={imageUrl}
-          alt={species.name}
-          width={128}
-          height={128}
-          priority
-          className="w-full object-scale-down"
-        />
-        <div className="pointer-events-none absolute -z-10 size-full scale-0 overflow-clip rounded-full transition-transform group-hover:scale-125 group-hover:bg-black dark:group-hover:bg-white p-6"></div>
-        <div className="absolute flex flex-col items-center justify-between opacity-0 group-hover:opacity-100 size-full transition-opacity">
-          <p
-            aria-hidden="true"
-            className="font-num text-lg text-zinc-400 dark:text-zinc-500"
-          >
-            {species.id}
-          </p>
-          <div className="flex flex-col items-center gap-2">
-            <h3 className="rounded-xs text-xl font-light text-white dark:text-black">
-              {name}
-            </h3>
-          </div>
-        </div>
-        {/* <div className="pointer-events-none absolute flex flex-col items-start justify-between">
-          <p
-            aria-hidden="true"
-            className="font-num text-sm text-zinc-400 dark:text-zinc-500"
-          >
-            {species.id}
-          </p>
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-row gap-2">
+      <div className="flex flex-col items-center justify-between overflow-hidden rounded-xl bg-white p-2 group-hover:bg-zinc-300 dark:bg-black dark:group-hover:bg-zinc-700">
+        <div className="relative flex w-full flex-col items-center rounded-md bg-zinc-100 p-4 dark:bg-zinc-900">
+          <Image
+            src={imageUrl}
+            alt={species.name}
+            width={128}
+            height={128}
+            priority
+            className="w-full object-scale-down"
+          />
+          <div className="absolute inset-x-0 top-0 flex h-full flex-col items-start justify-between rounded-lg p-2">
+            <p
+              aria-hidden="true"
+              className="font-num text-sm text-zinc-400 dark:text-zinc-500"
+            >
+              {species.id}
+            </p>
+            <div className="flex flex-row gap-2 opacity-0 group-hover:opacity-100">
               {types.map((typeResource) => (
                 <Image
                   key={typeResource.id}
@@ -94,13 +80,13 @@ export default async function MonsterCard({
                 />
               ))}
             </div>
-            <div className="flex w-full flex-row justify-between">
-              <h3 className="rounded-xs text-base font-light text-black dark:text-white">
-                {name}
-              </h3>
-            </div>
           </div>
-        </div> */}
+        </div>
+        <div className="flex w-full flex-row justify-between px-2 pt-2">
+          <h3 className="rounded-xs text-base font-light text-black dark:text-white">
+            {name}
+          </h3>
+        </div>
       </div>
     </Link>
   )
