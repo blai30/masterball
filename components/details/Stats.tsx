ab250f9cd12596f9971eb6cbde2a3b6662d15dde
Brian Lai <brianlai30@outlook.com>
2025-01-26 19:44:25 -0800

    Update tailwind to v4

--- components/details/Stats.tsx
+++ components/details/Stats.tsx
@@ -1,5 +1,6 @@
 import { Pokemon, Stat } from 'pokedex-promise-v2'
 import { getTranslation } from '@/lib/utils/pokeapiHelpers'
+import StatsRadarChart from '@/components/StatsRadarChart'
 
 const labelMap: Record<string, string> = {
   ['hp']: 'HP',
@@ -25,56 +26,65 @@ export default function Stats({
       <h2 className="text-xl font-medium text-black dark:text-white">
         {title}
       </h2>
-      <ul className="flex flex-col">
-        {stats.map((stat) => {
-          const name = getTranslation(stat.names, 'name')
-          const pokemonStat = pokemon.stats.find(
-            (s) => s.stat.name === stat.name
-          )!
-          const fillPercentage = ((pokemonStat.base_stat / 255) * 100).toFixed(
-            4
-          )
-          return (
-            <li key={stat.id} className="flex flex-row items-center gap-4">
-              <abbr
-                title={name}
-                aria-label={name}
-                className="min-w-20 font-normal text-black no-underline dark:text-white"
-              >
-                {labelMap[stat.name]}
-              </abbr>
-              <p className="font-num min-w-12 text-right text-black tabular-nums dark:text-white">
-                {pokemonStat.base_stat.toLocaleString()}
-              </p>
+      <div className="flex flex-row items-start gap-2">
+        <div className="">
+          {/* Bar chart */}
+          <ul className="flex flex-col">
+            {stats.map((stat) => {
+              const name = getTranslation(stat.names, 'name')
+              const pokemonStat = pokemon.stats.find(
+                (s) => s.stat.name === stat.name
+              )!
+              const fillPercentage = (
+                (pokemonStat.base_stat / 255) *
+                100
+              ).toFixed(4)
+              return (
+                <li key={stat.id} className="flex flex-row items-center gap-4">
+                  <abbr
+                    title={name}
+                    aria-label={name}
+                    className="min-w-20 font-normal text-black no-underline dark:text-white"
+                  >
+                    {labelMap[stat.name]}
+                  </abbr>
+                  <p className="font-num min-w-12 text-right text-black tabular-nums dark:text-white">
+                    {pokemonStat.base_stat.toLocaleString()}
+                  </p>
 
-              {/* Progress bar visualization */}
-              <div className="flex h-5 w-72 flex-row items-center">
-                <div className="h-full max-w-full flex-grow rounded-l-xs rounded-tr-xs rounded-br-md bg-zinc-200 dark:bg-zinc-900">
-                  <div
-                    className="h-full rounded-l-xs bg-black dark:bg-white"
-                    style={{
-                      width: `${fillPercentage}%`,
-                    }}
-                  ></div>
-                </div>
-              </div>
-            </li>
-          )
-        })}
-        <p>—</p>
-        <li className="flex flex-row items-center gap-4">
-          <abbr
-            title="Base stat total"
-            aria-label="Base stat total"
-            className="min-w-20 font-normal text-black no-underline dark:text-white"
-          >
-            Total
-          </abbr>
-          <p className="font-num min-w-12 text-right text-black tabular-nums dark:text-white">
-            {statTotal.toLocaleString()}
-          </p>
-        </li>
-      </ul>
+                  {/* Progress bar visualization */}
+                  <div className="flex h-5 w-72 flex-row items-center">
+                    <div className="h-full max-w-full flex-grow rounded-l-xs rounded-tr-xs rounded-br-md bg-zinc-200 dark:bg-zinc-900">
+                      <div
+                        className="h-full rounded-l-xs bg-black dark:bg-white"
+                        style={{
+                          width: `${fillPercentage}%`,
+                        }}
+                      ></div>
+                    </div>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+          <p>—</p>
+          <div className="flex flex-row items-center gap-4">
+            <abbr
+              title="Base stat total"
+              aria-label="Base stat total"
+              className="min-w-20 font-normal text-black no-underline dark:text-white"
+            >
+              Total
+            </abbr>
+            <p className="font-num min-w-12 text-right text-black tabular-nums dark:text-white">
+              {statTotal.toLocaleString()}
+            </p>
+          </div>
+
+          {/* Radar chart */}
+          <StatsRadarChart />
+        </div>
+      </div>
     </section>
   )
 }
