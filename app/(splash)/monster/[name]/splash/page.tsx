d7fead23148be70c7eb110821980b02181589ca2
Brian Lai <brianlai30@outlook.com>
2025-02-08 23:37:18 -0800

    Remove id number from splash

--- app/(splash)/monster/[name]/splash/page.tsx
+++ app/(splash)/monster/[name]/splash/page.tsx
@@ -33,42 +33,33 @@ export default async function Page({
   const imageId = species.id.toString().padStart(4, '0')
   const imageUrl = `https://resource.pokemon-home.com/battledata/img/pokei128/icon${imageId}_f00_s0.png`
 
-  const leadingZeros = imageId.match(/^0+/)?.[0] || ''
-  const significantDigits = imageId.slice(leadingZeros.length)
-
   return (
     <div className="h-[400px] w-[800px] bg-transparent">
       <GlassCard className="h-full w-full p-10">
         <div className="flex h-full flex-row justify-between">
-          <div className="flex flex-col items-start gap-4">
-            <p className="font-num relative text-3xl">
-              <span className="text-zinc-600">{leadingZeros}</span>
-              <span className="text-white">{significantDigits}</span>
-            </p>
-            <div className="flex h-full flex-col items-start justify-between">
-              <div className="flex flex-col items-start gap-6">
-                <h1 className="text-6xl font-semibold tracking-tight text-white">
-                  {label}
-                </h1>
-                <ul className="flex flex-row gap-2">
-                  {pokemon.types.map((type) => (
-                    <li key={type.type.name}>
-                      <TypePill variant={type.type.name} />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <Image
-                src={imageUrl}
-                alt={species.name}
-                width={128}
-                height={128}
-                priority
-                className="object-contain"
-              />
+          <div className="flex h-full flex-col items-start justify-between">
+            <div className="flex flex-col items-start gap-6">
+              <h1 className="text-6xl font-semibold tracking-tight text-white">
+                {label}
+              </h1>
+              <ul className="flex flex-row gap-2">
+                {pokemon.types.map((type) => (
+                  <li key={type.type.name}>
+                    <TypePill variant={type.type.name} />
+                  </li>
+                ))}
+              </ul>
             </div>
+            <Image
+              src={imageUrl}
+              alt={species.name}
+              width={128}
+              height={128}
+              priority
+              className="object-contain"
+            />
           </div>
-          <div className="flex h-full flex-col justify-center">
+          <div className="mr-3 flex h-full flex-col justify-center">
             <StatsRadarChart pokemon={pokemon} stats={stats} />
           </div>
         </div>
