d14b2f5f1bc86ee8ae4100d611fc84a612e04454
Brian Lai <brianlai30@outlook.com>
2025-02-19 13:36:19 -0800

    Reimplement theme switch style

--- components/shared/ThemeSwitch.tsx
+++ components/shared/ThemeSwitch.tsx
@@ -1,19 +1,24 @@
 'use client'
 
 import { useTheme } from 'next-themes'
+import { Monitor, Moon, Sun } from 'lucide-react'
+import { Radio, RadioGroup } from '@headlessui/react'
 
 const options = [
   {
     value: 'system',
-    label: 'System',
-  },
-  {
-    value: 'dark',
-    label: 'Dark',
+    label: 'System theme',
+    icon: <Monitor size={20} />,
   },
   {
     value: 'light',
-    label: 'Light',
+    label: 'Light theme',
+    icon: <Sun size={20} />,
+  },
+  {
+    value: 'dark',
+    label: 'Dark theme',
+    icon: <Moon size={20} />,
   },
 ]
 
@@ -21,19 +26,22 @@ export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <select
-      id="theme-switch"
-      name="Theme switch"
-      title="Theme switch"
+    <RadioGroup
       value={theme}
-      onChange={(e) => setTheme(e.target.value)}
-      className="bg-zinc-200 text-black dark:bg-zinc-800 dark:text-white"
+      onChange={setTheme}
+      title="Theme switch"
+      className="relative z-0 inline-grid grid-cols-3 gap-0.5 rounded-full bg-white/50 p-0.75 text-zinc-950 dark:bg-black/50 dark:text-white"
     >
       {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
+        <Radio
+          key={option.value}
+          value={option.value}
+          aria-label={option.label}
+          className="group relative flex size-7 cursor-default items-center justify-center rounded-full p-1 transition-colors data-checked:bg-black data-checked:text-white dark:data-checked:bg-white dark:data-checked:text-black"
+        >
+          {option.icon}
+        </Radio>
       ))}
-    </select>
+    </RadioGroup>
   )
 }
