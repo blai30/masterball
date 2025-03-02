12cb0051f52dac5c46749ae8790800a7d144d663
Brian Lai <brianlai30@outlook.com>
2025-03-02 14:26:38 -0800

    Update abilities styling

--- components/details/abilities/AbilitiesSection.tsx
+++ components/details/abilities/AbilitiesSection.tsx
@@ -31,42 +31,40 @@ export default async function AbilitiesSection({
       <h2 className="text-xl font-medium text-black dark:text-white">
         {title}
       </h2>
-      <ul className="flex flex-col gap-2">
+      <ul className="flex flex-col gap-4">
         {abilitiesMap.map((a) => {
           const name = getTranslation(a.resource.names, 'name')
           return (
             <li key={a.id} className="">
-              <div className="">
-                <div className="flex flex-col gap-2">
-                  <div className="flex flex-row items-center gap-2">
-                    <Link href={`/ability/${a.name}`} className="inline-block">
-                      <h3
-                        title={`${a.hidden ? 'Hidden ability' : `Ability ${a.slot}`}: ${name}`}
-                        className="font-medium text-blue-700 underline underline-offset-4 transition-colors hover:text-blue-800 hover:duration-0 dark:text-blue-300 dark:hover:text-blue-200"
-                      >
-                        {name}
-                      </h3>
-                    </Link>
-                    {a.hidden && (
-                      <span
-                        title="Hidden ability"
-                        className="rounded-md bg-zinc-300 px-2 py-0.5 dark:bg-zinc-700"
-                      >
-                        <EyeOff size={16} className="text-zinc-200" />
-                      </span>
-                    )}
-                  </div>
-                  <p className="text-lg text-pretty text-zinc-700 dark:text-zinc-300">
-                    {getTranslation(
-                      a.resource.effect_entries,
-                      'short_effect'
-                    ) ??
-                      getTranslation(
-                        a.resource.flavor_text_entries,
-                        'flavor_text'
-                      )}
-                  </p>
+              <div className="flex flex-col gap-1">
+                <div className="flex flex-row items-center gap-2">
+                  <Link href={`/ability/${a.name}`} className="inline-block">
+                    <h3
+                      title={`${a.hidden ? 'Hidden ability' : `Ability ${a.slot}`}: ${name}`}
+                      className="font-medium text-blue-700 underline underline-offset-4 transition-colors hover:text-blue-800 hover:duration-0 dark:text-blue-300 dark:hover:text-blue-200"
+                    >
+                      {name}
+                    </h3>
+                  </Link>
+                  {a.hidden && (
+                    <span
+                      title="Hidden ability"
+                      className="rounded-md px-2 inset-ring-1 inset-ring-zinc-800 dark:inset-ring-zinc-200"
+                    >
+                      <EyeOff
+                        size={16}
+                        className="text-zinc-800 dark:text-zinc-200"
+                      />
+                    </span>
+                  )}
                 </div>
+                <p className="text-lg text-pretty text-zinc-700 dark:text-zinc-300">
+                  {getTranslation(a.resource.effect_entries, 'short_effect') ??
+                    getTranslation(
+                      a.resource.flavor_text_entries,
+                      'flavor_text'
+                    )}
+                </p>
               </div>
             </li>
           )
