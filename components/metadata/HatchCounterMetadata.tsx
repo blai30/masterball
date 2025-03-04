a7c4ecf5cc349b11b0deee120250922f5ea039a9
Brian Lai <brianlai30@outlook.com>
2025-03-03 22:38:36 -0800

    Add undefined check to hatch counter

--- components/metadata/HatchCounterMetadata.tsx
+++ components/metadata/HatchCounterMetadata.tsx
@@ -1,12 +1,21 @@
 export default function HatchCounterMetadata({
   hatchCounter,
 }: {
-  hatchCounter: number
+  hatchCounter: number | undefined
 }) {
   const title = 'Hatch counter'
 
   // Multiply hatch cycles by number of steps in a cycle.
-  const hatchSteps = (hatchCounter * 128).toLocaleString()
+  const hatchSteps =
+    hatchCounter !== undefined ? (hatchCounter * 128).toLocaleString() : ''
+
+  if (!hatchCounter) {
+    return (
+      <div className="flex flex-col gap-2 rounded-lg">
+        <h2 className="text-sm/6 text-zinc-600 dark:text-zinc-400">{title}</h2>
+      </div>
+    )
+  }
 
   return (
     <div className="flex flex-col gap-2 rounded-lg">
@@ -22,7 +31,7 @@ export default function HatchCounterMetadata({
         </p>
         <p className="flex gap-x-1">
           <span className="text-base font-light text-black dark:text-white">
-            {hatchCounter.toLocaleString()}
+            {hatchCounter?.toLocaleString() ?? ''}
           </span>
           <span className="text-base text-zinc-600 dark:text-zinc-400">
             cycles
