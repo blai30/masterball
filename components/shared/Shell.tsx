08c736410e5aa81581e1e18e616ff4d3906b75f2
Brian Lai <brianlai30@outlook.com>
2025-03-03 18:34:57 -0800

    Disable Shell for now

--- components/shared/Shell.tsx
+++ components/shared/Shell.tsx
@@ -1,12 +1,4 @@
-import {
-  Ability,
-  EggGroup,
-  Item,
-  Move,
-  MoveDamageClass,
-  NamedAPIResource,
-  Type,
-} from 'pokedex-promise-v2'
+import { NamedAPIResource } from 'pokedex-promise-v2'
 import Header from '@/components/shared/Header'
 import {
   IndexItem,
@@ -28,146 +20,118 @@ import {
   TypeKey,
 } from '@/lib/utils/pokeapiHelpers'
 
-// Process data in smaller chunks to avoid memory issues
-async function fetchInChunks(
-  items: NamedAPIResource[],
-  fetchFn: Function,
-  chunkSize: number = 50
-) {
-  const results = []
-  for (let i = 0; i < items.length; i += chunkSize) {
-    const chunk = items.slice(i, i + chunkSize)
-    const chunkResults = await fetchFn(chunk.map((item) => item.name))
-    results.push(...chunkResults)
-  }
-  return results
-}
-
 export default async function Shell() {
-  const speciesList = await getTestSpeciesList()
-  const movesList = await getTestMovesList()
-  const abilitiesList = await getTestAbilitiesList()
-  const itemsList = await getTestItemsList()
-  const eggGroupsList = await pokeapi.getEggGroupsList()
+  // const speciesList = await getTestSpeciesList()
+  // const movesList = await getTestMovesList()
+  // const abilitiesList = await getTestAbilitiesList()
+  // const itemsList = await getTestItemsList()
+  // const eggGroupsList = await pokeapi.getEggGroupsList()
 
-  // Process data in chunks to prevent memory issues
-  const species = await fetchInChunks(
-    speciesList.results,
-    pokeapi.getPokemonSpeciesByName,
-    20
-  )
+  // const species = await pokeapi.getPokemonSpeciesByName(
+  //   speciesList.results.map((resource: NamedAPIResource) => resource.name)
+  // )
+  // const monsters = (await Promise.all(species.map(getMonstersBySpecies))).flat()
+  // const moves = await pokeapi.getMoveByName(
+  //   movesList.results.map((resource: NamedAPIResource) => resource.name)
+  // )
+  // const abilities = await pokeapi.getAbilityByName(
+  //   abilitiesList.results.map((resource: NamedAPIResource) => resource.name)
+  // )
+  // const items = await pokeapi.getItemByName(
+  //   itemsList.results.map((resource: NamedAPIResource) => resource.name)
+  // )
+  // const types = await pokeapi.getTypeByName(Object.values(TypeKey))
+  // const damageClass = await pokeapi.getMoveDamageClassByName(
+  //   Object.values(DamageClassKey)
+  // )
+  // const eggGroups = await pokeapi.getEggGroupByName(
+  //   eggGroupsList.results.map((resource: NamedAPIResource) => resource.name)
+  // )
 
-  // Limit number of monsters for development if needed
-  // const limitedSpecies = species.slice(0, 50); // Uncomment to limit species
-  // const monsters = (await Promise.all(limitedSpecies.map(getMonstersBySpecies))).flat();
-  const monsters = (await Promise.all(species.map(getMonstersBySpecies))).flat()
+  // const monsterItems: IndexItem[] = await Promise.all(
+  //   monsters.map(async (monster) => {
+  //     const { id, key, name, species, pokemon, form } = monster
+  //     const imageId = id.toString().padStart(4, '0')
+  //     const imageUrl =
+  //       pokemon.sprites.other?.home?.front_default ??
+  //       pokemon.sprites.other?.['official-artwork'].front_default ??
+  //       `https://resource.pokemon-home.com/battledata/img/pokei128/icon${imageId}_f00_s0.png`
+  //     const title = `${name} #${imageId}`
+  //     const path = form ? `/${species.name}/${form.name}` : `/${species.name}`
 
-  const moves: Move[] = await fetchInChunks(
-    movesList.results,
-    pokeapi.getMoveByName
-  )
-  const abilities: Ability[] = await fetchInChunks(
-    abilitiesList.results,
-    pokeapi.getAbilityByName
-  )
-  const items: Item[] = await fetchInChunks(
-    itemsList.results,
-    pokeapi.getItemByName
-  )
+  //     return {
+  //       id,
+  //       title,
+  //       slug: key,
+  //       path,
+  //       // imageUrl,
+  //     } as IndexItem
+  //   })
+  // )
 
-  const types: Type[] = await pokeapi.getTypeByName(Object.values(TypeKey))
-  const damageClass: MoveDamageClass[] = await pokeapi.getMoveDamageClassByName(
-    Object.values(DamageClassKey)
-  )
-  const eggGroups: EggGroup[] = await fetchInChunks(
-    eggGroupsList.results,
-    pokeapi.getEggGroupByName
-  )
+  // const moveItems: IndexItem[] = moves.map((move) => ({
+  //   id: move.id,
+  //   title: getTranslation(move.names, 'name')!,
+  //   slug: move.name,
+  //   path: `/move/${move.name}`,
+  // }))
 
-  const monsterItems: IndexItem[] = await Promise.all(
-    monsters.map(async (monster) => {
-      const { id, key, name, species, pokemon, form } = monster
-      const imageId = id.toString().padStart(4, '0')
-      const imageUrl =
-        pokemon.sprites.other?.home?.front_default ??
-        pokemon.sprites.other?.['official-artwork'].front_default ??
-        `https://resource.pokemon-home.com/battledata/img/pokei128/icon${imageId}_f00_s0.png`
-      const title = `${name} #${imageId}`
-      const path = form ? `/${species.name}/${form.name}` : `/${species.name}`
+  // const abilityItems: IndexItem[] = abilities.map((ability) => ({
+  //   id: ability.id,
+  //   title: getTranslation(ability.names, 'name')!,
+  //   slug: ability.name,
+  //   path: `/ability/${ability.name}`,
+  // }))
 
-      return {
-        id,
-        title,
-        slug: key,
-        path,
-        // imageUrl,
-      } as IndexItem
-    })
-  )
+  // const itemItems: IndexItem[] = items.map((item) => ({
+  //   id: item.id,
+  //   title: getTranslation(item.names, 'name')!,
+  //   slug: item.name,
+  //   path: `/item/${item.name}`,
+  // }))
 
-  const moveItems: IndexItem[] = moves.map((move) => ({
-    id: move.id,
-    title: getTranslation(move.names, 'name')!,
-    slug: move.name,
-    path: `/move/${move.name}`,
-  }))
+  // const typeItems: IndexItem[] = types.map((type) => ({
+  //   id: type.id,
+  //   title: TypeLabels[type.name as TypeKey],
+  //   slug: type.name,
+  //   path: `/type/${type.name}`,
+  // }))
 
-  const abilityItems: IndexItem[] = abilities.map((ability) => ({
-    id: ability.id,
-    title: getTranslation(ability.names, 'name')!,
-    slug: ability.name,
-    path: `/ability/${ability.name}`,
-  }))
+  // const damageClassItems: IndexItem[] = damageClass.map((damageClass) => {
+  //   return {
+  //     id: damageClass.id,
+  //     title: DamageClassLabels[damageClass.name as DamageClassKey],
+  //     slug: damageClass.name,
+  //     path: `/damage-class/${damageClass.name}`,
+  //   } as IndexItem
+  // })
 
-  const itemItems: IndexItem[] = items.map((item) => ({
-    id: item.id,
-    title: getTranslation(item.names, 'name')!,
-    slug: item.name,
-    path: `/item/${item.name}`,
-  }))
+  // const eggGroupItems: IndexItem[] = eggGroups.map((eggGroup) => {
+  //   const title = getTranslation(eggGroup.names, 'name')!
+  //   const imageUrl =
+  //     'https://resource.pokemon-home.com/battledata/img/pokei128/icon0000_f00_s0.png'
+  //   return {
+  //     id: eggGroup.id,
+  //     title,
+  //     slug: eggGroup.name,
+  //     path: `/egg-group/${eggGroup.name}`,
+  //     // imageUrl,
+  //   } as IndexItem
+  // })
 
-  const typeItems: IndexItem[] = types.map((type) => ({
-    id: type.id,
-    title: TypeLabels[type.name as TypeKey],
-    slug: type.name,
-    path: `/type/${type.name}`,
-  }))
-
-  const damageClassItems: IndexItem[] = damageClass.map((damageClass) => {
-    return {
-      id: damageClass.id,
-      title: DamageClassLabels[damageClass.name as DamageClassKey],
-      slug: damageClass.name,
-      path: `/damage-class/${damageClass.name}`,
-    } as IndexItem
-  })
-
-  const eggGroupItems: IndexItem[] = eggGroups.map((eggGroup) => {
-    const title = getTranslation(eggGroup.names, 'name')!
-    const imageUrl =
-      'https://resource.pokemon-home.com/battledata/img/pokei128/icon0000_f00_s0.png'
-    return {
-      id: eggGroup.id,
-      title,
-      slug: eggGroup.name,
-      path: `/egg-group/${eggGroup.name}`,
-      // imageUrl,
-    } as IndexItem
-  })
-
-  const allItems = [
-    ...monsterItems,
-    ...moveItems,
-    ...abilityItems,
-    ...itemItems,
-    ...typeItems,
-    ...damageClassItems,
-    ...eggGroupItems,
-  ]
+  // const allItems = [
+  //   ...monsterItems,
+  //   ...moveItems,
+  //   ...abilityItems,
+  //   ...itemItems,
+  //   ...typeItems,
+  //   ...damageClassItems,
+  //   ...eggGroupItems,
+  // ]
 
   return (
-    <GlobalIndexProvider indexItems={allItems}>
+    // <GlobalIndexProvider indexItems={allItems}>
       <Header />
-    </GlobalIndexProvider>
+    // </GlobalIndexProvider>
   )
 }
