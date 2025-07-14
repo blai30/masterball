a8177524440725d28b89f93c3dc670b6206d6954
Brian Lai <brianlai30@outlook.com>
2025-07-13 18:07:27 -0700

    Extract stats tab to separate client component with local storage persistence

--- components/details/stats/StatsSection.tsx
+++ components/details/stats/StatsSection.tsx
@@ -1,35 +1,7 @@
 import type { Pokemon } from 'pokedex-promise-v2'
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
-import { ChartBar, Hexagon } from 'lucide-react'
-import StatsBarChart from '@/components/details/stats/StatsBarChart'
-import StatsRadarChart from '@/components/details/stats/StatsRadarChart'
+import StatsTabs from '@/components/details/stats/StatsTabs'
 
 export default async function StatsSection({ pokemon }: { pokemon: Pokemon }) {
   const title = 'Stats'
-
-  return (
-    <section className="@container/stats flex flex-col gap-4 rounded-xl p-4 inset-ring-1 inset-ring-zinc-200 dark:inset-ring-zinc-800">
-      <TabGroup>
-        <TabList className="mb-4 flex flex-row items-center gap-1">
-          <h2 className="mr-3 text-xl font-medium text-black dark:text-white">
-            {title}
-          </h2>
-          <Tab className="rounded-md p-1.5 data-hover:inset-ring-2 data-hover:inset-ring-zinc-300 data-selected:bg-white dark:data-hover:inset-ring-zinc-700 dark:data-selected:bg-zinc-700">
-            <ChartBar />
-          </Tab>
-          <Tab className="rounded-md p-1.5 data-hover:inset-ring-2 data-hover:inset-ring-zinc-300 data-selected:bg-white dark:data-hover:inset-ring-zinc-700 dark:data-selected:bg-zinc-700">
-            <Hexagon />
-          </Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <StatsBarChart pokemon={pokemon} />
-          </TabPanel>
-          <TabPanel>
-            <StatsRadarChart pokemon={pokemon} />
-          </TabPanel>
-        </TabPanels>
-      </TabGroup>
-    </section>
-  )
+  return <StatsTabs title={title} pokemon={pokemon} />
 }
